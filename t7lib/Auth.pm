14da21e9a9fca26b2596c5f08e0fcfaf10c40f6a
Paul Miller <jettero@cpan.org>
2009-06-06 08:10:32 -0400

    auth and model skeletons

---
+++ t7lib/Auth.pm
@@ -0,0 +1,9 @@
+
+package t7lib::Auth;
+
+use strict;
+use warnings;
+
+use base 'Net::IMAP::Server::DefaultAuth';
+
+
---
+++ t7lib/Model.pm
@@ -0,0 +1,7 @@
+
+package t7lib::Model;
+
+use strict;
+use warnings;
+
+use base 'Net::IMAP::Server::DefaultModel';
