c383d739bd8951d59bed03ac5bfa5bbc406ddb6c
Paul Miller <jettero@cpan.org>
2009-06-10 21:53:41 -0400

    bug

--- contrib/slurp_fetchmail.pm
+++ contrib/slurp_fetchmail.pm
@@ -2,11 +2,12 @@
 package slurp_fetchmail
 
 use strict;
-use warnings
+use warnings;
 use Carp;
 use File::Slurp qw(slurp);
 
 sub login {
+    my $class = shift;
     my $fetchmailrc; slurp("$ENV{HOME}/.fetchmailrc");
     my ($server) = $fetchmailrc =~ m/server\s+(.+)/m;
     my ($user)   = $fetchmailrc =~ m/user\s+(.+)/m;
@@ -15,7 +16,7 @@ sub login {
     croak "server, user and pass must be in the $ENV{HOME}/.fetchmailrc for this to work"
         unless $server and $user and $pass;
 
-    my $imap = Net::IMAP::Simple->new($server, use_ssl=>1) or croak "connect failed: $Net::IMAP::Simple::errstr";
+    my $imap = Net::IMAP::Simple->new($server, @_) or croak "connect failed: $Net::IMAP::Simple::errstr";
        $imap->login($user=>$pass) or croak "login failed: " . $imap->errstr;
 
     return $imap;
