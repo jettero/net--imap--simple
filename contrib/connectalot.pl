8b8c1e4500735883e38258f65076e66aa236326c
Paul Miller <jettero@cpan.org>
2009-06-13 12:44:51 -0400

    made the connectalot script do more stuff

--- contrib/connectalot.pl
+++ contrib/connectalot.pl
@@ -4,13 +4,21 @@ use strict;
 use warnings;
 use IO::Socket::INET;
 
+my $ppid = $$;
+END { print "[$$] ", $$==$ppid ? "ppid ":"", "exit\n" };
+print "[$$] ppid started\n";
+
+$SIG{__WARN__} = sub { print "[$$] $_[0]" };
+$SIG{__DIE__}  = sub { print "[$$] $_[0]"; exit 0 };
+
 my @pids;
 for( 1 .. 5 ) {
     if( my $pid = fork ) {
         push @pids, $pid;
 
     } else {
-        my $sock = IO::Socket::INET->new("localhost:7000") or die "couldn't bind: $@";
+        print "[$$] start\n";
+        my $sock = IO::Socket::INET->new(PeerAddr=>"localhost:7000", Timeout=>2) or die "couldn't bind: $@";
         while( my $line = $sock->getline ) {
             print "[$$] $line";
         }
@@ -18,7 +26,7 @@ for( 1 .. 5 ) {
         my $eof = $sock->eof ? "EOF" : "...";
         my $ced = $sock->connected ? "CONNECTED" : "...";
 
-        print "[$$] eof: $eof; ced: $ced; exit()\n";
+        print "[$$] eof: $eof; ced: $ced\n";
         exit 0;
     }
 }
