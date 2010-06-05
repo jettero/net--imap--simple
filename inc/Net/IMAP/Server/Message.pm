abc7c597c62e3b08ca193230ffee2ab96b9ae032
Paul Miller <jettero@cpan.org>
2010-06-05 09:19:08 -0400

    I suspect this is not how alexmv will do it

--- inc/Net/IMAP/Server/Message.pm
+++ inc/Net/IMAP/Server/Message.pm
@@ -80,10 +80,16 @@ sub internaldate {
 
     if (ref $value) {
         $self->{internaldate} = $value->strftime("%e-%b-%Y %T %z");
+        $self->{internaldate} =~ s/^\s+//;
+        $self->{internaldate} =~ s/\s+$//;
+
     } else {
+        $value =~ s/^\s+//;
+        $value =~ s/\s+$//;
         $self->{internaldate} = $value;
         $value = $self->INTERNALDATE_PARSER->parse_datetime($value);
     }
+
     $value->truncate( to => "day" );
     $value->set_time_zone( "floating" );
     $value->set_time_zone( "UTC" );
