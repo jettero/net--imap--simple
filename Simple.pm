0b33f4d0142ed851cc608fe823e3b75e037b54bf
Paul Miller <jettero@cpan.org>
2010-07-31 09:21:59 -0400

    note changes

--- Simple.pm
+++ Simple.pm
@@ -8,7 +8,7 @@ use IO::File;
 use IO::Socket;
 use IO::Select;
 
-our $VERSION = "1.2010_99";
+our $VERSION = "1.2011_00";
 
 BEGIN {
     # I'd really rather the pause/cpan indexers miss this "package"
