ef40c2dc7ac388613735e2b8deeeb59b4ed5303b
Paul Miller <jettero@cpan.org>
2010-08-07 12:42:30 -0400

    only match BODY when it's not the middle of some other word.

--- contrib/SimpleX.pm
+++ contrib/SimpleX.pm
@@ -128,7 +128,7 @@ our $fetch_grammar = q&
 
     value_pair: tag value {$return=[$item[1], $item[2]]}
 
-    tag: /BODY(?:\.PEEK)?(?:\[[^\]]*\])?(?:<[\d\.]*>)?/i | atom
+    tag: /BODY\b(?:\.PEEK)?(?:\[[^\]]*\])?(?:<[\d\.]*>)?/i | atom
 
     value: atom | string | parenthized_list
 
