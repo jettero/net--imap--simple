1ffb97bb4fd94acd3e7a5fabf2fea19e8775c2b4
Paul Miller <jettero@cpan.org>
2010-08-29 15:17:43 -0400

    this clearly needs a lotta work, but it'll do

--- lib/Net/IMAP/Simple/PipeSocket.pm
+++ lib/Net/IMAP/Simple/PipeSocket.pm
@@ -5,17 +5,68 @@ use warnings;
 use Carp;
 use IPC::Open3;
 use IO::Select;
+use Symbol 'gensym';
+use base 'Tie::Handle';
+
+my %inn;
+my %out;
+my %err;
+my %pid;
 
 sub new {
     my $class = shift;
-    my $this  = bless(ref($_[0]) ? $_[0] : {@_}, $class);
+    my $cmd   = shift;
 
-    croak "cmd=>'eg ssh hostname dovecot' is a required argument" unless $this->{cmd};
+    croak "command (e.g. 'ssh hostname dovecot') argument required" unless $cmd;
 
-    my ($
+    open my $fake, "+>", undef or die "initernal error dealing with blarg: $!";
+
+    my($wtr, $rdr, $err); $err = gensym;
+    my $pid  = eval { open3($wtr, $rdr, $err, $cmd) } or croak $@;
+    my $this = bless $fake, $class or croak $!;
+
+    $inn{$this} = $rdr;
+    $out{$this} = $wtr;
+    $err{$this} = $err;
+    $pid{$this} = $pid;
 
     return $this;
 }
+
+sub UNTIE   { $_[0]->_waitpid }
+sub DESTROY { $_[0]->_waitpid }
+
+sub PRINT {
+    my $this = shift;
+    my $wtr  = $out{$this};
+
+    print $wtr @_;
+}
+
+sub READLINE {
+    my $this = shift;
+    my $rdr  = $inn{$this};
+
+    <$rdr>
+}
+
+sub _waitpid {
+    my $this = shift;
+
+    if( my $pid = delete $pid{$this} ) {
+        close delete $inn{$this} if exists $inn{$this};
+        close delete $out{$this} if exists $out{$this};
+        close delete $err{$this} if exists $err{$this};
+
+        waitpid( $pid, 0 );
+        my $child_exit_status = $? >> 8;
+        return $child_exit_status;
+    }
+
+    return;
+}
+
+1;
 
 __END__
 
