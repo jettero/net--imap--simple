134ea33350e1f2b1f6c94d31e904f9386d1f511c
Paul Miller <jettero@cpan.org>
2012-01-30 06:51:55 -0500

    version up

--- Simple.pm
+++ Simple.pm
@@ -9,7 +9,7 @@ use IO::Socket;
 use IO::Select;
 use Net::IMAP::Simple::PipeSocket;
 
-our $VERSION = "1.2028";
+our $VERSION = "1.2029";
 
 BEGIN {
     # I'd really rather the pause/cpan indexers miss this "package"
