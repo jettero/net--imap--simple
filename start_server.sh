2032a4163351b6bc766166f049e0807c13cb4d1c
Paul Miller <jettero@cpan.org>
2012-10-20 13:00:15 -0400

    these too, gone

--- start_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./stop_server.sh
-
-export SUICIDE_SECONDS=3600
-perl -Iblib/lib -w t/test_server.pm
--- stop_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for port in 19794 19795; do
-    fuser -k -n tcp $port
-done
-
-rm -vf imap_server.pid
