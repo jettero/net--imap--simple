ce7253a6ac0625f7f2b36bccbc6744ace3d87dea
Paul Miller <jettero@corky.vhb.voltar.org>
2017-04-24 11:05:21 -0400

    let's try this with sudo I guess

--- travis-blead.sh
+++ travis-blead.sh
@@ -5,8 +5,8 @@ set -x
 if [ "$TRAVIS_PERL_VERSION" = perl-blead ]; then
     if ! perlbrew use "$TRAVIS_PERL_VERSION"; then
 
-        perlbrew install perl-blead || exit 1
-        perlbrew use perl-blead     || exit 1
+        sudo perlbrew install perl-blead || exit 1
+        sudo perlbrew use perl-blead     || exit 1
     fi
 fi
 
