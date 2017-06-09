f9ca305c5630f035b0d5028c955cdcfacef4177f
Paul Miller <jettero@corky.vhb.voltar.org>
2017-06-09 06:59:22 -0400

    be talkative about reconfigs

--- config-imap.sh
+++ config-imap.sh
@@ -2,12 +2,15 @@
 
 apt-get install dovecot-imapd
 
+echo "#### RECONFIGURING DOVECOT for local virtual-user IMAP"
 sed -i -e 's/^[!]include auth/#!include auth/' \
        -e 's/^#!include auth-passwdfile/!include auth-passwdfile/' \
        /etc/dovecot/conf.d/10-auth.conf
 
+echo "#### CONFIGURING DOVECOT for local user=user password=password uid=$SUDO_UID gid=$SUDO_GID"
 echo "user:{plain}password:$SUDO_UID:$SUDO_GID::$PWD::userdb_mail=maildir:~/Maildir allow_nets=0.0.0.0/0" > /etc/dovecot/users
 chown -c dovecot:dovecot /etc/dovecot/users
 chmod -c 0640 /etc/dovecot/users
 
+echo "#### RESTARTING dovecot"
 service dovecot restart
