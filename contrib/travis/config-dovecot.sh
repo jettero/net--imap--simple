be265276561ddb75fc466592f493c4abbda3e0bd
Paul Miller <jettero@cpan.org>
2018-05-16 11:12:41 -0400

    hrm, dovecot isn't downloading, so this all breaks

--- contrib/travis/config-dovecot.sh
+++ contrib/travis/config-dovecot.sh
@@ -5,12 +5,12 @@ apt-get install dovecot-imapd
 echo "#### RECONFIGURING DOVECOT for local virtual-user IMAP"
 sed -i -e 's/^[!]include auth/#!include auth/' \
        -e 's/^#!include auth-passwdfile/!include auth-passwdfile/' \
-       /etc/dovecot/conf.d/10-auth.conf
+       /etc/dovecot/conf.d/10-auth.conf || exit 1
 
 echo "#### CONFIGURING DOVECOT for local user=user password=password uid=$SUDO_UID gid=$SUDO_GID"
 echo "user:{plain}password:$SUDO_UID:$SUDO_GID::$PWD::userdb_mail=maildir:~/Maildir allow_nets=0.0.0.0/0" > /etc/dovecot/users
-chown -c dovecot:dovecot /etc/dovecot/users
-chmod -c 0640 /etc/dovecot/users
+chown -c dovecot:dovecot /etc/dovecot/users || exit 1
+chmod -c 0640 /etc/dovecot/users || exit 1
 
 cat > /etc/dovecot/local.conf << EOF
 ssl = yes
@@ -21,10 +21,10 @@ EOF
 for i in dovecot.conf local.conf conf.d/10-auth.conf; do
     sed -e "s,^,# $i # ," /etc/dovecot/$i
 done
-sed -e 's/^/# local.conf   # /' /etc/dovecot/local.conf
+sed -e 's/^/# local.conf   # /' /etc/dovecot/local.conf || exit 1
 
 echo "#### RESTARTING dovecot"
-service dovecot restart
+service dovecot restart || exit 1
 
 sleep 1
-netstat -ntlp | sed -e 's/^/# netstat # /'
+netstat -ntlp | sed -e 's/^/# netstat # /' 
